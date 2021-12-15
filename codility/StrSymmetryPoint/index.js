050c0c9503dc29676f08d14ee85977e6b9be2b49
kimtaenam <cjb940103@gmail.com>
2021-12-16 00:51:46 +0900

    StrSymmetryPoint 2차 풀이

--- codility/StrSymmetryPoint/index.js
+++ codility/StrSymmetryPoint/index.js
@@ -1,11 +1,23 @@
 function solution(S) {
-    if(!S.length || S.length % 2 === 0) {
-        return -1;
-    };
-
     if(S.length === 1) {
         return 0;
     };
 
-    return Math.floor(S.length / 2);
-};
+    if(!S.length || S.length % 2 === 0) {
+        return -1;
+    };
+
+    let missCount = 0;
+
+    for (let i = 0; i< S.length; i++) {
+        if(S[i] !== S[S.length -1 -i]) {
+            missCount ++;
+        };
+    };
+
+    if(!missCount) {
+        return Math.floor(S.length / 2);
+    } else {
+        return -1;
+    };
+}
