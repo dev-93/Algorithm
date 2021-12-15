ef9f8cba1b5ada327b9248a1ee2c71b24d25f4d3
Kim Taenam <cjb940103@gmail.com>
2021-12-15 17:31:15 +0900

    2차 풀이

--- codility/OddOccurrencesInArray/index.js
+++ codility/OddOccurrencesInArray/index.js
@@ -1,16 +1,11 @@
 function solution(A) {
-    const countNum = A.reduce(function (allCounts, num) {
-        if (num in allCounts) {
-            allCounts[num]++;
-        } else {
-            allCounts[num] = 1;
-        }
-        return allCounts;
-    }, {});
-
-    for(let key in countNum){
-        if(Number(countNum[key]) === 1) {
-            return Number(key);
-        }
-    };
+    let element = new Set();
+    
+    for(let i in A){
+        !element.has(A[i]) ? element.add(A[i]) : element.delete(A[i]);
+    }
+    
+    const result = [...element];
+    
+    return result[0];
 }
