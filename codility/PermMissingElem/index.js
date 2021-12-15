29ee3ef4995451b30b15ad7e635dfb8d42c32e98
kimtaenam <cjb940103@gmail.com>
2021-12-15 21:23:49 +0900

    PermMissingElem 2차풀이

--- codility/PermMissingElem/index.js
+++ codility/PermMissingElem/index.js
@@ -1,9 +1,12 @@
 function solution(A) {
-    // write your code in JavaScript (Node.js 8.9.4)
-    if(!A.length) return 0;
+    if(!A.length) return 1;
 
     A.sort((a,b) => a-b);
-
+    
+    if (A.indexOf(1) === -1) {
+        return 1;
+    };
+    
     for (let i = 0; i < A.length; i++) {
         if(A[i] + 1 !== A[i+1]) {
             return A[i] + 1;
