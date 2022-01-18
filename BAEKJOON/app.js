41ba6cf8e9c5d650cf927109129dbefab18e7367
Kim Taenam <cjb940103@gmail.com>
2022-01-18 13:42:12 +0900

    백준 문제 초기화, 입력값만 받기

---
+++ BAEKJOON/app.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt').toString().split(' ').map(v => +v);
+// 백준
+// const input = fs.readFileSync("/dev/stdin").toString().split(' ').map(v => +v);
