318765ddb5ceb9623eba2c81011663c1617638ca
Kim Taenam <cjb940103@gmail.com>
2022-01-19 17:44:47 +0900

    더하기 사이클 문제 add

---
+++ BAEKJOON/1110/app.js
@@ -0,0 +1,8 @@
+// 더하기 사이클
+
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt').toString();
+// 백준
+// const input = fs.readFileSync("/dev/stdin").toString().split(' ').map(v => +v);
+
+console.log(input);
