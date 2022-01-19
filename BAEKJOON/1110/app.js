c3ffb437ada6a13be6d2fe2fe4b60633a6f3f6c5
Kim Taenam <cjb940103@gmail.com>
2022-01-19 17:57:55 +0900

    1110 문제풀이

--- BAEKJOON/1110/app.js
+++ BAEKJOON/1110/app.js
@@ -5,4 +5,15 @@ const input = fs.readFileSync('./input.txt').toString();
 // 백준
 // const input = fs.readFileSync("/dev/stdin").toString().split(' ').map(v => +v);
 
-console.log(input);
+let newNum = +input;
+
+let count = 0;
+
+while(newNum !== input) {
+    const arr = newNum.toString().split('');
+    const result = +arr[0] + +arr[1];
+    newNum = `${arr[1]}${result > 9 ? result - 10 : result}`;
+    count ++;
+}
+
+console.log(count);
