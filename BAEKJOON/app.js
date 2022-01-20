93fca8cb5cb00d3eab5fde03c5892f59ef2f5e8d
kimtaenam <cjb940103@gmail.com>
2022-01-21 02:13:05 +0900

    trim 추가

--- BAEKJOON/app.js
+++ BAEKJOON/app.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
-const input = fs.readFileSync('./input.txt').toString().split(' ').map(v => +v);
+const input = fs.readFileSync('./input.txt').toString().trim().split(' ').map(v => +v);
 // 백준
-// const input = fs.readFileSync("/dev/stdin").toString().split(' ').map(v => +v);
+// const input = fs.readFileSync("/dev/stdin").toString().trim().split(' ').map(v => +v);
