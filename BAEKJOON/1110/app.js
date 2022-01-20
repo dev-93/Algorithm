64ef424838a4536470f64102d94e571bbb7052cb
Kim Taenam <cjb940103@gmail.com>
2022-01-20 13:06:08 +0900

    input이 10미만일떄 고려

--- BAEKJOON/1110/app.js
+++ BAEKJOON/1110/app.js
@@ -1,19 +1,19 @@
-// 더하기 사이클
+let input = Number(require('fs').readFileSync('./input.txt').toString());
 
-const fs = require('fs');
-const input = fs.readFileSync('./input.txt').toString();
-// 백준
-// const input = fs.readFileSync("/dev/stdin").toString().split(' ').map(v => +v);
-
-let newNum = +input;
-
+let num = input;
+let sum;
 let count = 0;
 
-while(newNum !== input) {
-    const arr = newNum.toString().split('');
-    const result = +arr[0] + +arr[1];
-    newNum = `${arr[1]}${result > 9 ? result - 10 : result}`;
-    count ++;
+while (true) {
+	count++;
+    
+    // 주어진 수 합
+    sum = Math.floor(num / 10) + num % 10;
+    // 주어진 수의 오른쪽 자릿수 + 새로운 수의 오른쪽 자릿수
+    num = (num % 10) * 10 + sum % 10;
+    
+    // 주어진수 === 새로운 수 루프 탈출
+    if (input === num) break;
 }
 
 console.log(count);
