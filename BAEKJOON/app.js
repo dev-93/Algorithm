4b1355bcadc5052b72e1a53d594342e059ffaeaa
kimtaenam <cjb940103@gmail.com>
2022-01-21 23:58:09 +0900

    1157, 단어 공부 풀이

--- BAEKJOON/app.js
+++ BAEKJOON/app.js
@@ -1,4 +1,20 @@
-const fs = require('fs');
-const input = fs.readFileSync('./input.txt').toString().trim().split(' ').map(v => +v);
-// 백준
-// const input = fs.readFileSync("/dev/stdin").toString().trim().split(' ').map(v => +v);
+const fileLocation = "./input.txt";
+// const fileLocation = "/dev/stdin";
+
+const input = require('fs').readFileSync(fileLocation).toString().trim().split('\n');
+
+function solution(input) {
+    const answer = new Array(26).fill(0);
+    input = input[0].toUpperCase();
+
+    for(let i = 0; i < input.length; i++) {
+        answer[input[i].charCodeAt(0)-65] += 1;
+    };
+
+    const max = Math.max(...answer);
+    return answer.filter(v => v === max).length === 1 ?
+        String.fromCharCode(answer.findIndex(value => value === max) + 65)
+        : '?';
+}
+
+console.log(solution(input));
