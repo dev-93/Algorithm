5daf22b6b7e7cfa2acdad9097c009ece6eb8134f
kimtaenam <cjb940103@gmail.com>
2022-01-23 19:01:50 +0900

    초기화

--- BAEKJOON/app.js
+++ BAEKJOON/app.js
@@ -3,18 +3,4 @@ const fileLocation = "./input.txt";
 
 const input = require('fs').readFileSync(fileLocation).toString().trim().split('\n');
 
-function solution(input) {
-    const answer = new Array(26).fill(0);
-    input = input[0].toUpperCase();
-
-    for(let i = 0; i < input.length; i++) {
-        answer[input[i].charCodeAt(0)-65] += 1;
-    };
-
-    const max = Math.max(...answer);
-    return answer.filter(v => v === max).length === 1 ?
-        String.fromCharCode(answer.findIndex(value => value === max) + 65)
-        : '?';
-}
-
-console.log(solution(input));
+console.log(input);
