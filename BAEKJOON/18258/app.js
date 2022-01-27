c99ddb1346d7628d5dc007cf28edfcbf2a166c6f
Kim Taenam <cjb940103@gmail.com>
2022-01-27 13:27:48 +0900

    18258 수정

--- BAEKJOON/18258/app.js
+++ BAEKJOON/18258/app.js
@@ -1,38 +1,110 @@
 const fileLocation = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require('fs').readFileSync(fileLocation).toString().trim().split('\n');
 
-const input = require('fs').readFileSync(fileLocation).toString().split('\n');
+const [n, ...commands] = input;
 
-input.shift();
-
-const N = input.length;
-let answer = "";
-const stack = [];
-
-input.forEach((v, i) => {
-    const temp = v.split(" ");
-    switch(temp[0]) {
-        case 'front' :
-            stack.length < 1 ? answer += -1 : answer += stack[0];
-            break;
-        case 'back' :
-            stack.length < 1 ? answer += -1 : answer += stack[stack.length -1];
-            break;
-        case 'size' :
-            answer += stack.length;
-            break;
-        case 'pop' :
-            stack.length < 1 ? answer += -1 : answer += stack.shift();
-            break;
-        case 'empty' :
-            stack.length < 1 ? answer += 1 : answer += 0;
-            break;
-        default : 
-            stack.push(temp[1]);
-            break;
+class Node {
+    constructor(item) {
+        this.item = item;
+        this.next = null;
     }
-    if(i !== N - 1 && temp[0] !== 'push'){
-        answer+='\n'
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this._size = 0;
     }
-})
+
+    push(item) {
+        const node = new Node(item);
+        if(!this.head) {
+            this.head = node;
+            this.head.next = this.tail;
+        } else {
+            this.tail.next = node;
+        }
+        this.tail = node;
+        this._size ++;
+    }
+
+    size() {
+        return this._size;
+    }
+
+    pop() {
+        if(this._size > 2) {
+            const item = this.head.item;
+            const newHead = this.head.next;
+            this.head = newHead;
+            this._size --;
+            return item;
+        } else if(this._size === 2) {
+            const item = this.head.item;
+            const newHead = this.head.next;
+            this.head = newHead;
+            this.tail = newHead;
+            this._size --;
+            return item;
+        } else if (this._size === 1){
+            const item = this.head.item;
+            this.head = null;
+            this.tail = null;
+            this._size --;
+            return item;
+        } else {
+            return -1;
+        }
+    }
+
+    empty() {
+        return this.size ? 0 : 1;
+    }
+
+    front() {
+        return this.head ? this.head.item : -1;
+    }
+
+    back() {
+        return this.tail ? this.tail.item : -1;
+    }
+}
+
+
+function solution(n, commands) {
+    let answer = '';
+    const queue = new Queue();
+    
+    for (let i = 0; i < n; i++) {
+        const command = commands[i].split(' ');
+    
+        switch(command[0]){
+            case 'push':
+                queue.push(command[1])
+                break;
+            case 'pop':
+                answer.push(queue.pop());
+                break;
+            case 'size':
+                answer.push(queue.size())
+                break;
+            case 'empty':
+                answer.push(queue.empty())
+                break;
+            case 'front':
+                answer.push(queue.front())
+                break;
+            case 'back':
+                answer.push(queue.back())
+                break;
+        }
+    }
+
+    return answer.split(' ').join('\n');
+}
+
+
+const answer = solution(n, commands);
 
 console.log(answer);
